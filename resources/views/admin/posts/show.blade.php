f0eaf8d121a802596bce723e9f442f3c24c18475
RooRsmoka <chri.disa96@gmail.com>
2022-03-16 16:20:49 +0100

    created post views

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header d-flex">
+                    <a href="{{ route('admin.posts.index') }}" class="me-2"> < </a>
+                    {{ $post->title }}
+                    <a class="ms-auto" href="{{ route('admin.posts.edit', $post->slug) }}">Modifica</a>
+                </div>
+                <div class="card-body">
+                    <p class="lead">
+                        {!! $post->content !!}
+                    </p>
+                    <div class="my-3">
+                        Data creazione: {{ $post->created_at }}
+                        <br>
+                        Data ultima modifica: {{ $post->updated_at }}
+                        <br>
+                        Slug: {{ $post->slug }}
+                    </div>
+                    <div class="my-3">
+                        Utente: {{ $post->user->name }}
+                        <br>
+                        Email: {{ $post->user->email }}
+                    </div>
+                    @if ($post->category !== null)
+                    <div class="my-3">
+                        Categoria: {{ $post->category->code }}
+                        <br>
+                        Descrizione: {{ $post->category->description }}
+                    </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

0cdde6ec1f9ed75aba5a687f4fcc8c6ece33b208
RooRsmoka <chri.disa96@gmail.com>
2022-03-17 19:57:05 +0100

    modified route

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware('auth')
     ->group(function() {
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('posts', 'PostController');
-        Route::get("users", "UserController@index")->name("users.index");
+        Route::resource("users", "UserController");
     });
 
 Route::get('{any?}', function() {

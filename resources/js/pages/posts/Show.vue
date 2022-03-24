22690748e2d7a695e6c8c6a17abbbfb69b9e1687
RooRsmoka <chri.disa96@gmail.com>
2022-03-24 22:55:00 +0100

    created the post show page

---
+++ resources/js/pages/posts/Show.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h1>{{ post.title }}</h1>
+        <div class="mt-3 text-end d-flex align-items-center">
+            <span v-if="post.category" class="badge bg-dark me-2">
+                {{ post.category.name }}
+            </span>
+            <span v-for="tag in post.tags" :key="tag.id" class="badge bg-primary me-2">
+                {{ tag.name }}
+            </span>
+            <em v-if="post.user" class="ms-auto">
+                {{ post.user.name }}
+            </em>
+        </div>
+        <img :src="post.postImage" alt="" class="post-img img-fluid mt-3" />
+        <div v-html="post.content" class="mt-5 lead"></div>
+        <div class="mt-4">
+            <div>Data creazione: {{ post.created_at }}</div>
+            <div>Data ultimo aggiornamento: {{ post.updated_at }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            post: {}
+        }
+    },
+    mounted() {
+        console.log(this.$route.params.post);
+        this.fetchPost()
+    },
+    methods: {
+        async fetchPost() {
+            try {
+                const resp = await axios.get('/api/posts/' + this.$route.params.post);
+
+                this.post = resp.data;
+            } catch (er) {
+                this.$route.replace({ name: 'error' })
+            }
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+</style>

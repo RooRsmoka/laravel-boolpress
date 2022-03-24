20d2356077519090b96ae8a583057aa19272f915
RooRsmoka <chri.disa96@gmail.com>
2022-03-24 21:45:02 +0100

    changed id

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+    <div id="application">
         @include('partials.navbar')
 
         <main class="py-4">

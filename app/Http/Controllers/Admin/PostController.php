db8938a0345b2ecb4d1b6e06986e4529163cb8ac
RooRsmoka <chri.disa96@gmail.com>
2022-03-24 22:03:35 +0100

    imported trait

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Category;
+use App\Http\Controllers\Controller;
 use App\Post;
 use App\Tag;
-use App\Http\Controllers\Controller;
+use App\Traits\SlugGenerator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+    use SlugGenerator;
     /**
      * Display a listing of the resource.
      *
@@ -48,30 +49,23 @@ class PostController extends Controller
     {
         $data = $request->validate([
             'title' => 'required|min:5',
-            'content' => 'required|min:20',
+            'content' => 'required|min:30',
             'category_id' => 'nullable',
-            'tags' => 'nullable'
+            'tags' => 'nullable',
+            'postImage' => 'nullable|image|max:500'
         ]);
 
         $post = new Post();
         $post->fill($data);
-        $slug = Str::slug($post->title);
-        $exists = Post::where('slug', $slug)->first();
-        $counter = 1;
-
-        while ($exists) {
-            $newSlug = $slug . '-' . $counter;
-            $counter++;
-
-            $exists = Post::where('slug', $newSlug)->first();
-
-            if (!$exists) {
-                $slug = $newSlug;
-            }
-        }
+        $slug = $this->generateUniqueSlug($post->title);
 
         $post->slug = $slug;
         $post->user_id = Auth::user()->id;
+
+        if (key_exists('postImage', $data)) {
+            $post->postImage = Storage::put('postCovers', $data['postImage']);
+        }
+
         $post->save();
 
         if (key_exists('tags', $data)) {
@@ -120,9 +114,10 @@ class PostController extends Controller
     {
         $data = $request->validate([
             'title' => 'required|min:5',
-            'content' => 'required|min:20',
+            'content' => 'required|min:30',
             'category_id' => 'nullable|exists:categories,id',
-            'tags' => 'nullable|exists:tags,id'
+            'tags' => 'nullable|exists:tags,id',
+            'postImage' => 'nullable|image|max:500'
         ]);
 
         $post = Post::findOrFail($id);
@@ -133,6 +128,18 @@ class PostController extends Controller
         }
 
         $post->update($data);
+
+        if (key_exists('postImage', $data)) {
+
+            if ($post->postImage) {
+                Storage::delete($post->postImage);
+            }
+
+            $postImage = Storage::put('postCovers', $data['postImage']);
+
+            $post->postImage = $postImage;
+            $post->save();
+        }
 
         if (key_exists('tags', $data)) {
             $post->tags()->sync($data['tags']);
@@ -149,40 +156,20 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        $post = Post::findOrFail($id);
+        $post = Post::withTrashed()->findOrFail($id);
 
-        $post->tags()->detach();
+        if ($post->trashed()) {
+            $post->tags()->detach();
 
-        $post->delete();
+            if ($post->postImage) {
+                Storage::delete($post->postImage);
+            }
+
+            $post->forceDelete();
+        } else {
+            $post->delete();
+        }
 
         return redirect()->route('admin.posts.index');
     }
-
-
-    protected function generateUniqueSlug($postTitle) {
-        // Genero lo slug partendo dal titolo
-        $slug = Str::slug($postTitle);
-    
-        // controllo a db se esiste già un elemento con lo stesso slug
-        $exists = Post::where("slug", $slug)->first();
-        $counter = 1;
-    
-        // Fino a che $exists ha un valore diverso da null o false eseguo il while
-        while ($exists) {
-          // Genero un nuovo slug, prendendo quello precedente concatenato con il counter incrementato.
-          $newSlug = $slug . "-" . $counter;
-          $counter++;
-    
-          // controllo a db se esiste già un elemento con i nuovo slug appena generato
-          $exists = Post::where("slug", $newSlug)->first();
-    
-          // Se non esiste, salvo il nuovo slug nella variabile $slub che viene usato per assegnare
-          // il valore del nuovo post.
-          if (!$exists) {
-            $slug = $newSlug;
-          }
-        }
-    
-        return $slug;
-      }
 }

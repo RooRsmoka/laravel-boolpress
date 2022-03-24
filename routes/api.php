d176f94f13cf04eded7f24b9283802d24d9dac70
RooRsmoka <chri.disa96@gmail.com>
2022-03-24 21:41:15 +0100

    created routes and middleware for posts and contacts

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', 'Api\PostController@index');
+Route::get('/posts/{post}', 'Api\PostController@show');
+// Route::post('/posts', 'Api\PostController@store');
+Route::middleware('auth')->post('/posts', 'Api\PostController@store');
+Route::middleware('auth')->delete('/posts/{post}', 'Api\PostController@destroy');
+Route::post('/contacts', 'Api\ContactController@store');

1b2043536d61dfd4554bb0425a7a504620b5d282
RooRsmoka <chri.disa96@gmail.com>
2022-03-24 22:22:47 +0100

    added icons for user actions

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,35 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
+<div class="container">
     <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header d-flex">
-            Lista degli utenti
-          </div>
-
-          <table class="table">
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>Email</th>
-                {{-- <td></td> --}}
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($users as $user)
-                <tr>
-                  <td>{{ $user->name }}</td>
-                  <td>{{ $user->email }}</td>
-                  {{-- <td></td> --}}
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header d-flex">
+                    Lista degli utenti
+                </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Email</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <div class="ms-auto">
+                                <a class="me-2" href="{{ route('admin.users.show', $user->id) }}" title="View">
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
+                                <a class="text-secondary" href="{{ route('admin.users.edit', $user->id) }}" title="Edit">
+                                    <i class="fa-solid fa-edit"></i>
+                                </a>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  </div>
+</div>
 @endsection
